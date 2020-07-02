1c7a43caf6cae5b01478f833a74c77fc20ab85eb
qichao791 <66554627+qichao791@users.noreply.github.com>
2020-07-02 15:33:08 +0800

    Update BranchController.java
    update from cloud

--- src/main/java/com/example/demo/controller/BranchController.java
+++ src/main/java/com/example/demo/controller/BranchController.java
@@ -16,8 +16,6 @@ public class BranchController {
     @Autowired
     private BranchService branchService;
 
-    //opopop
-
     @GetMapping("/api/branch/getOneBranch")
     public Branch findOneBranch(@RequestParam(value = "branchId", required = true) String branchId) {
         return branchService.findOneById(branchId);
